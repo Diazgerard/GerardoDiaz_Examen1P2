17c512b0f0a89526f1d4600a73b0009938d46892
Diazgerard <gerardourraco234@gmail.com>
2022-05-20 13:54:29 -0600

    Inicio

---
+++ src/gerardodiaz_examen1p2/GerardoDiaz_Examen1P2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gerardodiaz_examen1p2;
+
+/**
+ *
+ * @author gerar
+ */
+public class GerardoDiaz_Examen1P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
